1915a434e9746d26aeca15706df0cb12cda1ff74
zijiegeng <ustcgzj@mail.ustc.edu.cn>
2025-04-15 23:20:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# LaMPlace: Learning to Optimize Cross-Stage Metrics in Macro Placement
+This repository contains the implementation of LaMPlace, which Learns a Mask for optimizing cross-stage metrics in macro placement.
+
+Paper: https://openreview.net/pdf?id=YLIsIzC74j
+
+**Note:** This is a pre-release version, and we are still in the process of organizing and refining the code. Updates with better organization, datasets, and detailed documentation will follow soon.
---
+++ common.py
@@ -0,0 +1,19 @@
+grid_setting = {
+    "superblue1": {"grid_num": 84, "grid_size": (535,196)},
+    "superblue3": {"grid_num": 84, "grid_size": (608,197)},
+    "superblue4": {"grid_num": 84, "grid_size": (354,197)},
+    "superblue5": {"grid_num": 84, "grid_size": (589,271)},
+    "superblue7": {"grid_num": 84, "grid_size": (379,339)},
+    "superblue10": {"grid_num": 84, "grid_size": (518,368)},
+    "superblue16": {"grid_num": 84, "grid_size": (353,192)},
+    "superblue18": {"grid_num": 84, "grid_size": (289,192)},
+    "ariane133": {"grid_num": 84, "grid_size": (90,90)},
+    "ariane136": {"grid_num": 84, "grid_size": (90,90)},
+    "bp_be": {"grid_num": 84, "grid_size": (48,42)},
+    "bp": {"grid_num": 84, "grid_size": (80,83)},
+    "bp_fe": {"grid_num": 84, "grid_size": (48,36)},
+    "bp_multi": {"grid_num": 84, "grid_size": (67,67)},
+    "swerv_wrapper": {"grid_num": 84, "grid_size": (67,61)},
+}
+
+my_inf = 1e12
